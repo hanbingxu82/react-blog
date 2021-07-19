5e1a576c0b0f914e1c9aaaceb5b549f3899c7c02
hanbingxu <hbx831@126.com>
2021-07-19 09:32:01 +0800

    style:2021-07-19  调整前端 兼容样式   手机端右上角git图标定位 fixed等

--- src/components/GitHub/GitHub.tsx
+++ src/components/GitHub/GitHub.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-14 18:46:17
- * @LastEditTime: 2021-07-14 18:57:45
+ * @LastEditTime: 2021-07-19 09:27:00
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /blogreact/src/components/GitHub/GitHub.tsx
@@ -12,7 +12,7 @@ class GitHub extends Component {
   render() {
     return (
       <a href="https://github.com/hanbingxu82/react-blog" className="github-corner" target="_blank" title="Follow me on GitHub" rel="noopener noreferrer" aria-label="Follow me on GitHub">
-        <svg width="80" height="80" viewBox="0 0 250 250" style={{ fill: "#f3543a", color: "#fff", position: "absolute", top: "0", border: "0", right: "0" }} aria-hidden="true">
+        <svg width="80" height="80" viewBox="0 0 250 250" style={{ fill: "#f3543a", color: "#fff", position: "fixed", top: "0", border: "0", right: "0" }} aria-hidden="true">
           <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
           <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style={{transformOrigin:"130px 106px"}} className="octo-arm"></path>
           <path
